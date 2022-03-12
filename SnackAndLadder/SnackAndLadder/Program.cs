c6332a89bb4c2b446883beeb34062f89d4be610e
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-12 16:18:16 +0530

    Uc2 :Roll the die get number

--- SnackAndLadder/SnackAndLadder/Program.cs
+++ SnackAndLadder/SnackAndLadder/Program.cs
@@ -1,14 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(" Welcome to Snake And Ladder ");
-
 {
-    int position = 0;
-
-
-    Console.WriteLine("Single Player");
-
-    Console.WriteLine(" Start position:" + position );
-
-
-
+    Random random = new Random();
+    int CheckDie = random.Next(1, 7);
+    Console.WriteLine("Die number:" + CheckDie);
 }
