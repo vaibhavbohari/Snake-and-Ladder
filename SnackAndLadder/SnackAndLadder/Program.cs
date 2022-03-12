35798403735892c15b6eda7fe80eb4b321e39f1d
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-12 15:51:26 +0530

    Uc1 :player is 0 position

--- SnackAndLadder/SnackAndLadder/Program.cs
+++ SnackAndLadder/SnackAndLadder/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(" Welcome to Snake And Ladder ");
+
+{
+    int position = 0;
+
+
+    Console.WriteLine("Single Player");
+
+    Console.WriteLine(" Start position:" + position );
+
+
+
+}
